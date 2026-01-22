09e494997d0fd24c38ba50a849dab5e41b4c462c
BerndSeggewiss <81984865+BerndSeggewiss@users.noreply.github.com>
2026-01-22 19:44:16 +0100

    Add files via upload

---
+++ Arduino sketch.ino
@@ -0,0 +1,14 @@
+const int analogPin;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(analogPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  Serial.println(voltage, 3);
+  delay(500);
+}
